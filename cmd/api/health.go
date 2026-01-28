a155ac43aaefc71c24752229352951cf21b3d7e3
Sergey Savelyev <itsgotrunn@gmail.com>
2026-01-28 18:47:41 +0300

    added air hot reload

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("health"))
+	w.Write([]byte("health test"))
 }
