38dac938425e8f0c92bc4f5eee688f24c3dc95df
Sergey Savelyev <itsgotrunn@gmail.com>
2026-01-28 18:12:13 +0300

    chi added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+)
+
+func main() {
+	cfg := config{
+		addr: ":8080",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+	log.Fatal(app.run(mux))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module training-platform
 
 go 1.24.0
+
+require github.com/go-chi/chi/v5 v5.2.4 // indirect
