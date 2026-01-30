340738d84ae024c47eb0db6eacfc01df9a76018b
Sergey Savelyev <itsgotrunn@gmail.com>
2026-01-30 15:01:44 +0300

    added db config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"training-platform/internal/db"
 	"training-platform/internal/env"
 	"training-platform/internal/store"
 
@@ -16,9 +17,26 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
