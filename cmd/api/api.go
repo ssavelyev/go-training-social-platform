2db899b8d8d87b593dbbb873eaf87031d4e9267f
Sergey Savelyev <itsgotrunn@gmail.com>
2026-02-02 17:29:56 +0300

    added errors validation

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -39,6 +39,14 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.getPostHandler)
+			})
+		})
 	})
 
 	return r
