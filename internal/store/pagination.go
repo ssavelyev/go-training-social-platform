f22b0ee9edaad9dadb4958538ac82d9861ebd366
Sergey Savelyev <itsgotrunn@gmail.com>
2026-02-08 16:13:00 +0300

    updated filters

--- internal/store/pagination.go
+++ internal/store/pagination.go
@@ -3,12 +3,18 @@ package store
 import (
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type PaginationQuery struct {
-	Limit  int    `json:"limit" validate:"gte=1,lte=20"`
-	Offset int    `json:"offset" validate:"gte=0"`
-	Sort   string `json:"sort" validate:"oneof=asc desc"`
+	Limit     int      `json:"limit" validate:"gte=1,lte=20"`
+	Offset    int      `json:"offset" validate:"gte=0"`
+	Sort      string   `json:"sort" validate:"oneof=asc desc"`
+	Tags      []string `json:"tags" validate:"max=5"`
+	Search    string   `json:"search" validate:"max=100"`
+	StartDate string
+	EndDate   string
 }
 
 func (fq PaginationQuery) Parse(r *http.Request) (PaginationQuery, error) {
@@ -24,9 +30,9 @@ func (fq PaginationQuery) Parse(r *http.Request) (PaginationQuery, error) {
 		fq.Limit = limit
 	}
 
-	OffsetQuery := qs.Get("offset")
-	if OffsetQuery != "" {
-		offset, err := strconv.Atoi(OffsetQuery)
+	offsetQuery := qs.Get("offset")
+	if offsetQuery != "" {
+		offset, err := strconv.Atoi(offsetQuery)
 		if err != nil {
 			return fq, err
 		}
@@ -34,10 +40,39 @@ func (fq PaginationQuery) Parse(r *http.Request) (PaginationQuery, error) {
 		fq.Offset = offset
 	}
 
-	SortQuery := qs.Get("sort")
-	if SortQuery != "" {
-		fq.Sort = SortQuery
+	sortQuery := qs.Get("sort")
+	if sortQuery != "" {
+		fq.Sort = sortQuery
+	}
+
+	tagsQuery := qs.Get("tags")
+	if tagsQuery != "" {
+		fq.Tags = strings.Split(tagsQuery, ",")
+	}
+
+	searchQuery := qs.Get("search")
+	if searchQuery != "" {
+		fq.Search = searchQuery
+	}
+
+	startDateQuery := qs.Get("startDate")
+	if startDateQuery != "" {
+		fq.StartDate = parseTime(startDateQuery)
+	}
+
+	endDateQuery := qs.Get("endDate")
+	if endDateQuery != "" {
+		fq.EndDate = parseTime(endDateQuery)
 	}
 
 	return fq, nil
+}
+
+func parseTime(str string) string {
+	t, err := time.Parse(time.DateTime, str)
+	if err != nil {
+		return ""
+	}
+
+	return t.Format(time.DateTime)
 }
